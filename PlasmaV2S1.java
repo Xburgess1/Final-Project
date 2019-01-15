7054aeabf8593ca40def571eff7421bfa276b308
Xburgess1 <43046974+Xburgess1@users.noreply.github.com>
2019-01-15 15:31:59 -0600

    New updated version of the game

--- PlasmaV2S1.java
+++ PlasmaV2S1.java
@@ -1,32 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class BulletV1S1 here.
+ * Write a description of class PlasmaV2S1 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class BulletV1S1 extends Bullets
+public class PlasmaV2S1 extends Bullets
 {
-    //private int for bullet speed
-    private int speed = 6;
     /**
-     * Act sets its own location 6 less then it's own y location. If the bullet reaches the edge of the world it removes itself
+     * Act - Act sets its own location 6 less then it's own y location. If the plasma bullet reaches the edge of the world it removes itself
      * 
      * @param None there are no parameters
      * @return there is no return
      */
     public void act() 
     {
-        //the bullet does 1 damage to most enemies
+        //The plasma bullet does 1.5 damage to most enemies
         
-        
-        setLocation(getX(), getY()-speed);
-        
+        setLocation(getX(), getY() -6);
         
         if(isAtEdge() == true)
         {
             getWorld().removeObject(this);
         }
+        
+        
     }    
 }
